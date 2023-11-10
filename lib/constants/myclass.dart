d0815ca35ee71400733f357ca6584adb0b1ae3cb
salemakowe <salemakowe@gmail.com>
2023-11-10 15:03:09 +0100

    setup

---
+++ lib/constants/myclass.dart
@@ -0,0 +1,12 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+class LocationData {
+  String? countryName;
+  String? city;
+  String? condition;
+  double? lat;
+  double? long;
+  int? temp;
+  int? humidity;
+}
