6455c6b9ab32e86f8945b927a181d3b4e71adf27
salemakowe <salemakowe@gmail.com>
2023-11-11 17:26:38 +0100

    still setting up

---
+++ lib/constants/myassets.dart
@@ -0,0 +1,3 @@
+class MyAssets {
+  static const location = 'assets/icons/location.png';
+}
---
+++ lib/constants/mytexts.dart
@@ -0,0 +1,5 @@
+class MyTexts {
+  static const apiKey = 'b91a781eec5d3983adc30755ddd7e3b6';
+  static const apiUrl =
+      'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=$apiKey';
+}
